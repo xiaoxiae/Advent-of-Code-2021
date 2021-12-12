ffbe18c28e103d5bc17ddb0677dffda68abb0947
xiaoxiae <tomas@slama.dev>
2021-12-12 13:46:46 +0100

    day 12

---
+++ 12/12-1.py
@@ -0,0 +1,50 @@
+import sys
+
+sys.path.insert(0, "../")
+from utilities import success, get_input
+
+input = get_input()
+
+caves = {}
+
+
+def is_small(x):
+    return x == x.lower()
+
+
+for u, v in map(lambda x: x.split("-"), input):
+    if u not in caves:
+        caves[u] = []
+    if v not in caves:
+        caves[v] = []
+
+    caves[u].append(v)
+    caves[v].append(u)
+
+
+total = 0
+
+
+def count_distinct_paths(current, visited):
+    global total
+
+    if current == "end":
+        total += 1
+        return
+
+    for neighbour in caves[current]:
+        if neighbour == "start" or neighbour in visited:
+            continue
+
+        if is_small(neighbour):
+            visited.add(neighbour)
+
+        count_distinct_paths(neighbour, visited)
+
+        if is_small(neighbour):
+            visited.remove(neighbour)
+
+
+count_distinct_paths("start", set())
+
+success(total)
